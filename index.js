613554eddae1493dd545450763e201d6f6013ee3
York Wang <yw544@nyu.edu>
2017-11-22 07:05:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ var newID = oldID + 1000000000
 var ageIsValid = Number.isInteger(currentAge)
 
 var randomNumber = Math.floor(Math.random() *20) + 1
+
+var randomInteger = Math.round(randomNumber);
+
+var randomUserID = 
